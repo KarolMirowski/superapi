598f94ca6d5da238cca46d8367c7878f64d3a3b8
KarolMirowski <karolpoczta@protonmail.com>
2023-05-02 11:17:34 +0200

    New file
    Character.cs, first model class

---
+++ Models/Character.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace superapi.Models;
+
+
+public class Character
+{
+    public int Id { get; set; }
+}
