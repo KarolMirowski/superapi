1ac5ef96ca1d15c05657058a3af0a25d0610a979
KarolMirowski <karolpoczta@protonmail.com>
2023-05-05 15:26:53 +0200

    Entity Framework Core
    Has been added

--- Program.cs
+++ Program.cs
@@ -2,13 +2,17 @@ global using superapi.Models;
 global using superapi.Services;
 global using superapi.Dtos.Character;
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
+global using superapi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
